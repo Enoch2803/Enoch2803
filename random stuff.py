a05931318ae45a1aaaa1b4fd0426fa346fe3128e
Enoch <167523159+Enoch2803@users.noreply.github.com>
2024-08-21 09:56:57 +0100

    random stuff.py

---
+++ random stuff.py
@@ -0,0 +1,11 @@
+name = input('Name: ')
+age = input('age: ')
+gender = input('gender: ')
+location = input('location: ')
+school = input('school: ')
+favourite_food  = input('favourite_food: ')
+
+
+
+print('Hello your name is', name, 'and you are approximately', age, 'years old,', 'and you are a', gender, 'if I am not mistaken')
+print('You are in the', location, 'region and have attended', school, 'as part of your previous education')
